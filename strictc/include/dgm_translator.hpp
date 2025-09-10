a00f7b74792189838111c9f1747066fa29a180c9
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-09 23:50:54 -0400

    Add DGMTranslator class for file translation

---
+++ strictc/include/dgm_translator.hpp
@@ -0,0 +1,9 @@
+# strictc/include/dgm_translator.hpp
+#pragma once
+#include <string>
+#include <vector>
+
+class DGMTranslator {
+public:
+    void translate(const std::string& inFile, const std::string& outFile);
+};
